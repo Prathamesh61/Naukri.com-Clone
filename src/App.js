65339bf3420367a2d4b75d985d1146b9338f8a72
Prathamesh <rawoolprathamesh61@gmail.com>
2022-08-26 03:53:23 +0530

    Day-3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { LandingPage, TopCompanies } from './components/landing Page/LandingPage';
+import { Register } from './components/Register';
 
 function App() {
   return (
     <div >
+      <Register />
       <LandingPage />
     </div>
   );
